5b85bb74c4aaf7255af9d370971d7ce790aa126d
ivan <vanyaveresov123@gmail.com>
2023-03-26 21:52:14 +0300

    Функция сортировки слиянием

--- my_labs/lab6/lab6.2.py
+++ my_labs/lab6/lab6.2.py
@@ -1,31 +1,56 @@
 def merge(A, left, mid, right):
-    # копируем сюда функцию merge из упражнения 6.1
-    pass
+    # вспомогательный массив, в который будут сливаться элементы
+    AUX = []
+    # Инициализируем указатели i и j
+    i = left
+    j = mid + 1
 
-def mergeSort(A, left = 0, right = None, verbose = False):
-    # если параметр right == None, то это первый вызов и надо исправить его на реальное значение
-    # if ...
+    # Цикл, осуществляющий слияние
+    while i <= mid and j <= right:
+        if A[i] <= A[j]:
+            AUX.append(A[i])
+            i += 1
+        else:
+            AUX.append(A[j])
+            j += 1
 
+    # Дописываем хвост (почитайте справку к функции extend для списков) и не забываем про indexes
+    AUX.extend(A[i: mid + 1])
+    AUX.extend(A[j: right + 1])
+    # Возвращаем назад в массив A результат нашей работы (обратите внимание на присваивание срезу!)
+    A[left:right + 1] = AUX
+
+def mergeSort(A, left=0, right=None, verbose=False):
+    #если параметр right == None, то это первый вызов и надо исправить его на реальное значение
+    if right == None:
+        right = len(A) - 1
     # если массив пустой или состоит всего из одного элемента, заканчиваем
-    # if ...
+    if left >= right:
+        return A
 
     # определяем середину
-    # mid = ...
+    mid = (left + right) // 2
 
     # рекурсивно сортируем обе половины
-    # ...
-
+    mergeSort(A, left, mid, verbose)
+    mergeSort(A, mid + 1, right, verbose)
     # печатаем массив и производим слияние с помощью функции merge
-    # ...
+    Ast = list(map(str, A))
+    if verbose:
+        for i in range(len(A)):
+            if i == left:
+                Ast[i] = '[' + Ast[i]
+            if i == mid:
+                Ast[i] = Ast[i] + ']'
+            if i == mid + 1:
+                Ast[i] = '[' + Ast[i]
+            if i == right:
+                Ast[i] = Ast[i] + ']'
+    print(' '.join(Ast))
+    merge(A, left, mid, right)
 
 # читаем список A (и возможно слово 'verbose' на второй строке)
-# ...
-# вызываем mergeSort и не забываем напечатать результат его работы еще раз!
-# ...
-
-"""
-Прочитать слово verbose "обычным" способом не получится, т.к. в 80% тестов второй строки нет и ваша программа сломается при попытке ее чтения. Прочитать то, чего может и не быть, можно с помощью обработки исключений. Изучите и используйте код, приведенный ниже:
-
+A = list(map(int, input().split()))
 try:
     verbose = input()
     if verbose == 'verbose':
@@ -33,4 +58,6 @@ try:
 except EOFError:
     verbose = False
 
-"""
+# вызываем mergeSort и не забываем напечатать результат его работы еще раз!
+mergeSort(A, verbose= verbose)
+print(' '.join(list(map(str, A))))
