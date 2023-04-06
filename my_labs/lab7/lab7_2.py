5f52ffea1becc6fbb1666a87416abaaef6bc0c4e
ivan <vanyaveresov123@gmail.com>
2023-04-06 23:29:24 +0300

    Написал интерполяционный поиск вместе с тестами.

--- my_labs/lab7/lab7_2.py
+++ my_labs/lab7/lab7_2.py
@@ -1,18 +1,33 @@
 def interpolSearch(A, elem):
+    hi = len(A) - 1
     # если искомый элемент за границами диапазона,то делать нечего
-    if elem ==
+    if elem  < A[0] or elem > A[hi]:
+        return None
 
     # определим верхнюю границу и вызовем рекурсивную функцию
-    # ...
+    return interpolSearchRec(A, elem, 0, hi)
 
-def interpolSearchRec(A, elem, lo, hi):
+def interpolSearchRec(A, elem, lo, hi, string=None):
     # если подмассив пустой ИЛИ elem за границами диапазона, то делать нечего
-    # if ...
+    if string == None:
+        string = []
+    if lo > hi or elem < A[lo] or elem > A[hi]:
+        return ' '.join(string)
 
     # если левая и правая границы совпадают, то mid по формуле вычислять нельзя! (почему?)
-    # if ...
+    if A[hi] == A[lo]:
+        mid = lo
     # иначе - вычисляем mid по формуле
-    # mid = ...
+    else:
+        mid = lo + round((elem - A[lo]) * ((hi - lo) / (A[hi] - A[lo])))
 
     # выполняем сравнение и рекурсивный вызов на одной из частей
-    # if ...
+    if elem == A[mid]:
+        string.append(str(A[mid]))
+        return ' '.join(string)
+    if elem < A[mid]:
+        string.append(str(A[mid]))
+        return interpolSearchRec(A, elem, lo, mid - 1, string)
+    else:
+        string.append(str(A[mid]))
+        return interpolSearchRec(A, elem, mid + 1, hi, string)
--- my_labs/lab7/tests/test7_2.py
+++ my_labs/lab7/tests/test7_2.py
@@ -1,2 +1,32 @@
 import unittest
-from my_labs.lab7.lab7_2 import
+from my_labs.lab7.lab7_2 import interpolSearch
+from my_labs.lab7.lab7_2 import interpolSearchRec
+
+class Test_interpol_search(unittest.TestCase):
+    def test_normal(self):
+        A =[1, 4, 7, 8, 10, 17, 21, 23, 26, 32, 36, 40, 41, 43, 44, 47, 49]
+        self.assertEqual(interpolSearch(A, 8), '7 8')
+
+
+    def test_negative(self):
+        A = [2, 4, 7, 9, 15, 16, 20, 21, 22, 28, 29]
+        self.assertEqual(interpolSearch(A, 13), '15')
+
+
+    def test_same_elements(self):
+        A = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+        self.assertEqual(interpolSearch(A, 1), '1')
+
+
+    def test_negative_more(self):
+        A = [1, 5, 10, 21, 45, 64]
+        self.assertEqual(interpolSearch(A, 65), None)
+
+
+    def test_negative_less(self):
+        A = [13, 14, 16, 56, 64]
+        self.assertEqual(interpolSearch(A, 12), None)
+
+    def test_string_except(self):
+        with self.assertRaises(TypeError) as e:
+            interpolSearch('abcdefgh', 'e')
