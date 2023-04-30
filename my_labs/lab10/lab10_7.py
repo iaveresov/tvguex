ef7a9184cc4bef88e2e6e72cf7f6dd274b945eae
ivan <vanyaveresov123@gmail.com>
2023-05-01 00:46:10 +0300

    Процедура погружения кучи с тестами

---
+++ my_labs/lab10/lab10_7.py
@@ -0,0 +1,53 @@
+# для метода sift_down понадобятся вспомогательные методы left_son, right_son, min_son
+# не забудьте про аннотирование типов!
+def left_son(p: int) -> int:
+    # возвращаем индекс левого сына элемента p
+    return 2 * p + 1
+
+
+def right_son(p: int) -> int:
+    # возвращаем индекс правого сына элемента p
+    return 2 * p + 2
+
+
+def min_son(p: int, heap: list[int, ...], lenght: int) -> int:
+    # возвращаем индекс минимального сына элемента p или -1, если p - лист
+    l: int = left_son(p)
+    r: int = right_son(p)
+    tail: int = lenght - 1
+    if l > tail:
+        return -1
+    if l < tail and r <= tail:
+        minim: int = min(heap[l], heap[r])
+        if heap[l] == minim:
+            return l
+        else:
+            return r
+    elif l == tail:
+        return l
+
+def sift_down(heap: list[int, ...], p: int) -> None:
+    lenght: int = len(heap)
+    minCh: int = min_son(p, heap, lenght)
+    # пока мы не в листе и текущий элемент больше минимального из сыновей,
+    # меняем их местами и погружаемся ниже
+    while minCh > 0 and heap[minCh] < heap[p]:
+        heap[minCh]: int
+        heap[p]: int
+        heap[minCh], heap[p] = heap[p], heap[minCh]
+        p: int = minCh
+        minCh: int = min_son(p, heap, lenght)
+
+
+if __name__ == '__main__':
+    # считать массив heap
+    heap: list[int, ...] = list(map(int, input().split()))
+
+    # считать индекс всплываемого элемента
+    p: int = int(input())
+
+    # осуществляем всплытие
+    sift_down(heap, p)
+
+    # напечатать heap
+    print(' '.join(list(map(str, heap))))
---
+++ my_labs/lab10/tests/test10_7.py
@@ -0,0 +1,20 @@
+import unittest
+from my_labs.lab10.lab10_7 import left_son, right_son, min_son, sift_down
+
+
+class Test(unittest.TestCase):
+    def test_positive1(self):
+        heap = [15, 10, 11, 13, 11, 14, 26, 16, 17, 14]
+        sift_down(heap, 0)
+        self.assertEqual(heap, [10, 11, 11, 13, 14, 14, 26, 16, 17, 15])
+
+    def test_postitive2(self):
+        heap = [1, 2, 4, 6, 5, 10, 24, 6, 7, 8, 10, 11, 23, 21, 25]
+        sift_down(heap, 6)
+        self.assertEqual(heap, [1, 2, 4, 6, 5, 10, 21, 6, 7, 8, 10, 11, 23, 24, 25])
+
+
+    def test_positive(self):
+        heap = [1, 2, 4, 6, 5, 10, 24, 6, 7, 8, 10, 11, 23, 21]
+        sift_down(heap, 6)
+        self.assertEqual(heap, [1, 2, 4, 6, 5, 10, 21, 6, 7, 8, 10, 11, 23, 24])
